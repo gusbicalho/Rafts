0e9d86861619a2e56be7641a7c5ee06be7ee6f8c
Gustavo Bicalho <gusbicalho@gmail.com>
2019-03-05 07:06:16 +0000

    add Get* requests from Client

--- lib/types/Client.ts
+++ lib/types/Client.ts
@@ -1,4 +1,4 @@
-import { ClientId, Message, MemberId } from "./Basic";
+import { ClientId, Message, MemberId, LogIndex, Entry } from "./Basic";
 
 export interface ProduceMessageRequest {
     type: 'Event/ProduceMessageRequest'
@@ -16,3 +16,38 @@ export interface ProduceMessageRejectedResponse {
     clientId: ClientId
     leaderId: MemberId
 }
+
+export interface GetFromRequest {
+    type: 'Event/GetFromRequest'
+    clientId: ClientId
+    from: LogIndex
+    count: BigInt
+}
+
+export interface GetFromResponse {
+    type: 'Effect/GetFromResponse'
+    clientId: ClientId
+    entries: Entry[]
+}
+
+export interface GetLatestRequest {
+    type: 'Event/GetLatestRequest'
+    clientId: ClientId
+}
+
+export interface GetLatestResponse {
+    type: 'Effect/GetLatestResponse'
+    clientId: ClientId
+    entry: Entry
+}
+
+export type ClientEffect =
+    | ProduceMessageAcceptedResponse
+    | ProduceMessageRejectedResponse
+    | GetFromResponse
+    | GetLatestResponse
+
+export type ClientEvent =
+    | ProduceMessageRequest
+    | GetFromRequest
+    | GetLatestRequest
